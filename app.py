2ee83aed90f4aa87e3910958d690371294393df7
pcssn.PIEthonista <pcssn.yxxx@gmail.com>
2024-12-20 01:57:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -91,4 +91,5 @@ def process_data() -> dict[str, str]:
     print("Results after binary classification: ", result.item())
     return jsonify(result.item())
 
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
