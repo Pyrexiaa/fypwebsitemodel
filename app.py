ac685d9bb103d57e07c9c5e8ff3f04a48252e7b4
Jia Yu Lim <lim.jia.yu.desmond@gmail.com>
2024-12-19 16:10:37 +0800

    update port

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
@@ -93,4 +95,5 @@ def process_data() -> dict[str, str]:
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(debug=True, host="0.0.0.0", port=port)
